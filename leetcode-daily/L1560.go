60eee230621351188c7d5bf44727d65cbf282f1e
PansonPanson <PansonPanson@163.com>
2022-09-01 11:20:17 +0800

    L1560

--- leetcode-daily/L1560.go
+++ leetcode-daily/L1560.go
@@ -1,5 +1,24 @@
 package main
 
-func main() {
-
+/**
+1560.在既定时间做作业的学生人数(Easy)
+*/
+func busyStudent(startTime []int, endTime []int, queryTime int) int {
+	res := 0
+	for i, v := range startTime {
+		if queryTime >= v && queryTime <= endTime[i] {
+			res++
+		}
+	}
+	return res
 }
+
+//func busyStudent(startTime []int, endTime []int, queryTime int) int {
+//	res := 0
+//	for i := 0; i < len(startTime); i++ {
+//		if startTime[i] < queryTime && queryTime > endTime[i] {
+//			res++
+//		}
+//	}
+//	return res
+//}
