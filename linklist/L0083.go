8499c789e1acb0bd623299f16f1adc828dcd3db2
PansonPanson <PansonPanson@163.com>
2023-02-12 18:18:23 +0800

    L0083

---
+++ linklist/L0083.go
@@ -0,0 +1,17 @@
+package linklist
+
+// LeetCode 83. 删除排序链表中的重复元素
+func deleteDuplicates(head *ListNode) *ListNode {
+	if head == nil {
+		return nil
+	}
+	cur := head
+	for cur.Next != nil {
+		if cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+		} else {
+			cur = cur.Next
+		}
+	}
+	return head
+}
